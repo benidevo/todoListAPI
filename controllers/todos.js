37462fedce81cdc10c2b354b0beb4a523aa73b4e
Benjamin Idewor <benjaminidewor@gmail.com>
2021-11-30 21:43:48 +0100

    feat: update, delete, and retrieve task by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -12,3 +12,61 @@ exports.getAllTodoItems = async function (req, res) {
     const todoItems = await TodoItem.find();
     res.status(200).json({tasks: todoItems});
 };
+
+
+exports.getTodoItemById = async function (req, res) {
+    const taskId = req.params.taskId
+
+    let todoItem;
+    try {
+        todoItem = await TodoItem.findById(taskId)
+    } catch (error) {
+        return res.status(404).json({ message: 'Task not found' });
+    }
+
+    if (!todoItem) {
+        return res.status(404).json({ message: 'Task not found' });
+    }
+
+    res.status(200).json({ data: todoItem });
+}
+
+exports.deleteTodoItem = async function (req, res) {
+    const taskId = req.params.taskId;
+
+    let todoItem;
+    try {
+        todoItem = await TodoItem.findById(taskId)
+    } catch (error) {
+        return res.status(404).json({ message: 'Task not found' });
+    }
+
+    if (!todoItem) {
+        return res.status(404).json({ message: 'Task not found' });
+    }
+
+    await todoItem.delete();
+    res.status(200).json({ message: 'Task deleted' });
+}
+
+exports.updateTodoItem = async function (req, res) {
+    const task = req.body.task;
+    const taskId = req.params.taskId;
+
+    let todoItem;
+    try {
+        todoItem = await TodoItem.findById(taskId)
+    } catch (error) {
+        return res.status(404).json({ message: 'Task not found' });
+    }
+
+    if (!todoItem) {
+        return res.status(404).json({ message: 'Task not found' });
+    }
+
+    todoItem.task = task;
+    todoItem.updatedAt = Date.now();
+    await todoItem.save();
+
+    res.status(200).json({ data: todoItem });
+};
--- routes/todos.js
+++ routes/todos.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {createTodoItem, getAllTodoItems} = require('../controllers/todos');
+const {createTodoItem, getAllTodoItems, getTodoItemById, deleteTodoItem, updateTodoItem} = require('../controllers/todos');
 
 // create todo item
 router.post('/', createTodoItem)
 
 // retrieve all todo items
 router.get('/', getAllTodoItems)
+
+// retrieve todo item by id
+router.get('/:taskId', getTodoItemById)
+
+// delete todo item by id
+router.delete('/:taskId', deleteTodoItem)
+
+// update todo item by id
+router.put('/:taskId', updateTodoItem)
 
 module.exports = router;
